2cbe567b6a3e3655c8403f9263cb1d9bd3e1fe8b
jgg77 <113386349+jgg77@users.noreply.github.com>
2022-09-12 22:06:09 +0200

    project added

---
+++ FutbolScoreBoard/FutbolScoreBoard/Class1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FutbolScoreBoard
+{
+    public class BoardManagement
+    {
+        public struct Team 
+        {
+            public Team(string Name, int Result) 
+            {
+                name = Name;
+                result = Result;
+            }
+            public string name { get; set; }
+            public int result { get; set; }
+        }
+        public Dictionary<Team,Team> BoardLive;
+        public Dictionary<Dictionary<Team,Team>, DateTime> SummaryBoard;
+
+        public BoardManagement() 
+        {
+            BoardLive = new Dictionary<Team, Team>();
+        }
+        public bool addGame(Dictionary<Team, Team> board, Team homeTeam, Team awayTeam) 
+        {
+            try
+            {
+                board.Add(homeTeam, awayTeam);
+                if (board.ContainsKey(homeTeam) & board.ContainsValue(awayTeam))
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception EX)
+            {
+                return false;
+            }
+        }
+        public bool removeGame(Dictionary<Team, Team> board,Team homeTeam)
+        {
+            try
+            {
+                board.Remove(homeTeam);
+                if (!board.ContainsKey(homeTeam))
+                    return true;
+                else
+                    return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool UpdateGame(Dictionary<Team, Team> board, Team local, Team away)
+        {
+            try
+            {
+                bool teamUpdated = false;
+                
+                foreach (var TeamLoop in board)
+                {
+                    if (TeamLoop.Key.name.Equals(local.name))
+                    {
+                        teamUpdated = true;
+                        removeGame(board, local);
+                        addGame(board, local, away);
+                        break;
+                        
+                    }
+                    if (!teamUpdated & TeamLoop.Value.name.Equals(local.name))
+                    {
+                        teamUpdated = true;
+                        removeGame(board, local);
+                        addGame(board, local, away);
+                        break;
+
+                    }
+                }
+                if (teamUpdated)
+                    return true;
+                else 
+                {
+                    Console.WriteLine("Something was wrong trying to update the result");
+                    return false;
+                }
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool publishBoard(Dictionary<Team, Team> Board)
+        {
+            try
+            {
+                foreach (var game in Board)
+                    Console.WriteLine(String.Format("{0} - {1}: {2} - {3}", game.Key.name,game.Value.name,game.Key.result,game.Value.result));
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ FutbolScoreBoard/FutbolScoreBoard/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace FutbolScoreBoard
+{
+    class Program: BoardManagement
+    {
+        static void Main()
+        {
+            BoardManagement manage = new BoardManagement();
+            Team localTeam = new Team("",0);
+            Team awayTeam = new Team("", 0);
+            Team teamInfoUpdate = new Team();
+            string[] args = Console.ReadLine().Split(',');
+            do
+            {
+                switch (args[0])
+                {
+                    case "ADD":
+                        localTeam.name = args[1];
+                        localTeam.result = Convert.ToInt32(args[2]);
+                        awayTeam.name = args[3];
+                        awayTeam.result = Convert.ToInt32(args[4]);
+                        if (manage.addGame(manage.BoardLive, localTeam, awayTeam))
+                            Console.WriteLine("Added succesfully");
+                        else
+                            Console.WriteLine("Cannot add the game");
+                        break;
+                    case "UPDATE":
+                        localTeam.name = args[1];
+                        localTeam.result = Convert.ToInt32(args[2]);
+                        awayTeam.name = args[3];
+                        awayTeam.result = Convert.ToInt32(args[4]);
+                        if (manage.UpdateGame(manage.BoardLive, localTeam,awayTeam))
+                            Console.WriteLine("Updated succesfully");
+                        else
+                            Console.WriteLine("Cannot update the game");
+                        break;
+                    case "REMOVE":
+                        localTeam.name = args[1];
+                        localTeam.result = Convert.ToInt32(args[2]);
+                        if (manage.removeGame(manage.BoardLive, localTeam))
+                            Console.WriteLine("Updated succesfully");
+                        else
+                            Console.WriteLine("Cannot update the game");
+                        break;
+                    case "PUBLISH":
+                        if (manage.publishBoard(manage.BoardLive))
+                            Console.WriteLine("Done");
+                        else
+                            Console.WriteLine("Cannot publish the board");
+                        break;
+                    default:
+                        break;
+                }
+                args = Console.ReadLine().Split(',');
+            } while (!args[0].Equals("FINISH"));
+
+        }
+    }
+}
